9b0c766505c6ef24464c12eeb374d3b6ed596bbe
Max-mich <61765273+Max-mich@users.noreply.github.com>
2021-04-14 16:01:11 +0200

    class

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function Document(numEnreg, titre) {
+/* function Document(numEnreg, titre) {
   this.numEnreg = numEnreg;
   this.titre = titre;
 }
@@ -32,11 +32,11 @@ Livre.prototype = Object.create(Document.prototype);
 Revue.prototype = Object.create(Document.prototype);
 Dictionnaire.prototype = Object.create(Document.prototype);
 let livre = new Livre(100, "Bob", "Max", 150);
-console.log(livre);
+console.log(livre); */
 
 // Change random Color
 
-const texte = document.querySelector("p span");
+/* const texte = document.querySelector("p span");
 texte.style.color = "#FF0000";
 
 function ajoute() {
@@ -45,4 +45,49 @@ function ajoute() {
 }
 function supprime() {
   texte.style.color = "#FF0000";
+} */
+
+class Document {
+  constructor(numEnreg, titre) {
+    this.numEnreg = numEnreg;
+    this.titre = titre;
+  }
 }
+class Livre extends Document {
+  constructor(numEnreg, titre, auteur, nbrPages) {
+    super(numEnreg, titre);
+    this.auteur = auteur;
+    this.nbrPages = nbrPages;
+    if (nbrPages < 100) {
+      console.log("Nombre de page entre 0 et 100");
+    } else if (nbrPages > 101 && nbrPages < 200) {
+      console.log("Nombre de page entre 101 et 200");
+    } else if (nbrPages > 200 && nbrPages < 300) {
+      console.log("Nombre de page entre 200 et 300");
+    } else if (nbrPages > 301) {
+      console.log("Nombre de page supérieur à 301");
+    } else {
+      console.log("Pas de pages");
+    }
+  }
+}
+class Revue extends Document {
+  constructor(numEnreg, titre, mois, annee) {
+    super(numEnreg, titre);
+    this.mois = mois;
+    this.annee = annee;
+  }
+}
+class Dictionnaire extends Document {
+  constructor(numEnreg, titre, langue) {
+    super(numEnreg, titre);
+    this.langue = langue;
+  }
+}
+
+let livre = new Livre(5, "Test", "Maxime", 15);
+console.log(livre);
+let revue = new Revue(5, "Revue", 05, 2017);
+console.log(revue);
+let dictonnaire = new Dictionnaire(2, "Robert", "Eng");
+console.log(dictonnaire);
